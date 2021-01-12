caa8e225619f1ca51b34fc9e4d297c8f289df8b7
caiojulian <caiojulian@hotmail.com>
2021-01-12 15:53:26 -0400

    plugin axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+//import './plugins/axios'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
@@ -8,7 +9,7 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-Vue.filter('formatPrice', function(value) {
+Vue.filter('formatPrice', function (value) {
   return parseFloat(value).toFixed(2).replace('.', ',')
 })
 
