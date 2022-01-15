3e494fdb30342a92657c48f46b21e4e18c22c050
Caio <caiojulian@hotmail.com>
2022-01-15 00:48:39 -0300

    feat: component cart

---
+++ src/components/Cart.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-container>
+    <b-col cols="12" class="text-justify">
+      <h4 class="font-weight-bold text-primary">Carrinho</h4>
+    </b-col>
+    <b-row class="col-12">
+      <b-col cols="6">
+        <p class="text-left">Taxa de instalação</p>
+      </b-col>
+      <b-col cols="6">
+        <p class="text-right">Grátis</p>
+      </b-col>
+    </b-row>
+
+    <b-row class="col-12" v-for="(item, index) in items" :key="index">
+      <b-col cols="6">
+        <p class="text-left">{{ item.title }}</p>
+      </b-col>
+      <b-col cols="6">
+        <p class="text-right">R$ {{ item.price | formatPrice }}</p>
+      </b-col>
+    </b-row>
+
+    <b-row class="col-12 font-weight-bold text-primary">
+      <b-col cols="6">
+        <p class="text-left">Total</p>
+      </b-col>
+      <b-col cols="6">
+        <p class="text-right">R$ {{ total | formatPrice }}/mês</p>
+      </b-col>
+    </b-row>
+
+    <b-row class="col-12 pt-2">
+      <b-col cols="6">
+        <b-button class="px-4" @click="cleanCart"
+          >Limpar Carrinho</b-button
+        >
+      </b-col>
+      <b-col cols="6 text-right">
+        <b-button
+          variant="primary"
+          class="px-4"
+          :disabled="isDisabled"
+          @click="confirmSolicitation"
+          >Confirmar</b-button
+        >
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  props: {
+    isDisabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    items() {
+      return this.$store.state.carrinho.items;
+    },
+    total() {
+      return this.$store.getters.total;
+    }
+  },
+  methods: {
+    confirmSolicitation() {
+      alert("Solicitação\nConfirmar solicitação de compra?");
+      this.cleanCart();
+    },
+    cleanCart() {
+      this.$store.dispatch("resetStateAll");
+      this.$store.commit("resetItems");
+    }
+  }
+};
+</script>
--- src/components/Loja.vue
+++ src/components/Loja.vue
@@ -28,60 +28,15 @@
     <hr class="col-sm-11 col-md-12 my-4" />
 
     <!-- Carrinho -->
-    <b-col cols="12" class="text-justify">
-      <h4 class="font-weight-bold text-primary">Carrinho</h4>
-    </b-col>
-    <b-row class="col-12">
-      <b-col cols="6">
-        <p class="text-left">Taxa de instalação</p>
-      </b-col>
-      <b-col cols="6">
-        <p class="text-right">Grátis</p>
-      </b-col>
-    </b-row>
-
-    <b-row class="col-12" v-for="(item, index) in items" :key="index">
-      <b-col cols="6">
-        <p class="text-left">{{ item.title }}</p>
-      </b-col>
-      <b-col cols="6">
-        <p class="text-right">R$ {{ item.price | formatPrice }}</p>
-      </b-col>
-    </b-row>
-
-    <b-row class="col-12 font-weight-bold text-primary">
-      <b-col cols="6">
-        <p class="text-left">Total</p>
-      </b-col>
-      <b-col cols="6">
-        <p class="text-right">R$ {{ total | formatPrice }}/mês</p>
-      </b-col>
-    </b-row>
-
-    <b-row class="col-12 pt-2">
-      <b-col cols="6">
-        <b-button class="px-4" @click="limparCarrinho"
-          >Limpar Carrinho</b-button
-        >
-      </b-col>
-      <b-col cols="6 text-right">
-        <b-button
-          variant="primary"
-          class="px-4"
-          :disabled="isDisabled"
-          @click="confirmarSolicitacao"
-          >Confirmar</b-button
-        >
-      </b-col>
-    </b-row>
+    <cart :isDisabled="isDisabled" />
   </b-row>
 </template>
 
 <script>
+import Cart from "./Cart.vue";
 import NewCard from "./NewCard.vue";
 export default {
-  components: { NewCard },
-  name: "Compras",
+  components: { NewCard, Cart },
   created() {
     this.$store.dispatch("getLists");
   },
@@ -95,28 +50,17 @@ export default {
     list_fixo() {
       return this.$store.state.list_fixo;
     },
-    items() {
-      return this.$store.state.carrinho.items;
-    },
-    total() {
-      return this.$store.getters.total;
-    },
     isDisabled() {
       let disabled = this.$store.getters.disabledList;
-      if (disabled) this.limparCarrinho();
+      if (disabled) this.cleanCart();
       return disabled;
-    },
+    }
   },
   methods: {
-    limparCarrinho() {
+    cleanCart() {
       this.$store.dispatch("resetStateAll");
       this.$store.commit("resetItems");
-    },
-    confirmarSolicitacao() {
-      alert("Solicitação\nConfirmar solicitação de compra?");
-      this.limparCarrinho();
-    },
-  },
+    }
+  }
 };
 </script>
-
